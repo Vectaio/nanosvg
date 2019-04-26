910049febd8dfe4dbe6e6cb96890cf5ae3c25cec
neel <neel2292@gmail.com>
2019-04-26 10:01:47 +0800

    Minor changes

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ var Nano = require('nanosvg'),
     nano = new Nano({ key: <YOUR API KEY> });
 
 // compress in bulk
-nano.compressFiles('./*.svg', './compressed/');
+nano.compress('./*.svg', './compressed/');
 ```
 ## Usage on CLI
 
@@ -28,88 +28,6 @@ nano.compressFiles('./*.svg', './compressed/');
 ### Compress files
 
 `nanosvg --key <YOUR API KEY> ./*.svg ./compressed/`
-
-## API
-
-## Objects
-
-<dl>
-<dt><a href="#Nano">Nano</a> : <code>object</code></dt>
-<dd><p>Nano client constructor</p>
-</dd>
-</dl>
-
-## Typedefs
-
-<dl>
-<dt><a href="#File">File</a> : <code>object</code></dt>
-<dd><p>SVG file object</p>
-</dd>
-</dl>
-
-<a name="Nano"></a>
-
-## Nano : <code>object</code>
-Nano client constructor
-
-**Kind**: global namespace  
-
-| Param | Type | Default | Description |
-| --- | --- | --- | --- |
-| opts | <code>object</code> |  | Options to initialize Nano |
-| opts.key | <code>string</code> |  | API key to initialize Nano |
-| [opts.mode] | <code>number</code> | <code>0</code> | Compression mode for Nano. Can be 0 = image mode or 1 = object mode |
-
-
-* [Nano](#Nano) : <code>object</code>
-    * [.compress(file)](#Nano+compress) ⇒ [<code>Promise.&lt;File&gt;</code>](#File)
-    * [.compressFiles(src, tgt)](#Nano+compressFiles)
-
-<a name="Nano+compress"></a>
-
-### nano.compress(file) ⇒ [<code>Promise.&lt;File&gt;</code>](#File)
-Compress a SVG in string format and returns compress SVG in string format
-
-**Kind**: instance method of [<code>Nano</code>](#Nano)  
-
-| Param | Type | Description |
-| --- | --- | --- |
-| file | <code>string</code> \| [<code>File</code>](#File) | Can be either a string representing a SVG or an object with said string with size |
-
-**Example**  
-```js
-var Nano = require('nanosvg'),    nano = new Nano({ key: <YOUR API KEY> });    compress('<svg>...</svg>').then(function(res){        console.log(res); // { str: '<svg>...</svg>', size: ... }    });
-```
-<a name="Nano+compressFiles"></a>
-
-### nano.compressFiles(src, tgt)
-Compress svg(s) from a directory and output to a target directory
-
-**Kind**: instance method of [<code>Nano</code>](#Nano)  
-
-| Param | Type | Description |
-| --- | --- | --- |
-| src | <code>string</code> | Glob pattern representing source files. |
-| tgt | <code>string</code> | Glob pattern representing target directory. |
-
-**Example**  
-```js
-var Nano = require('nanosvg'),    nano = new Nano({ key: <YOUR API KEY> });    nano.compressFiles('./uncompressed/*.svg', './compressed/');    // Compressed: 0.svg, 0/2    //  Savings: 38.87%    // Compressed: 1.svg, 1/2    //  Savings: 70.32%    // Compressed: 2.svg, 2/2    //  Savings: 69.99%
-```
-<a name="File"></a>
-
-## File : <code>object</code>
-SVG file object
-
-**Kind**: global typedef  
-**Properties**
-
-| Name | Type | Default | Description |
-| --- | --- | --- | --- |
-| string | <code>string</code> |  | SVG in string format |
-| [size] | <code>number</code> |  | Size of SVG |
-| [mode] | <code>number</code> | <code>0</code> | Compression mode |
-
 
 ## License
 
--- index.js
+++ index.js
@@ -10,162 +10,99 @@ var CWD = process.cwd(),
     URL = 'https://api.vecta.io/nano',
     nano;
 
-/**
- * SVG file object
- * @typedef {object} File
- * @property {string} string SVG in string format
- * @property {number} [size] Size of SVG
- * @property {number} [mode=0] Compression mode
- */
-
-/**
- * Nano client constructor
- * @param {object} opts Options to initialize Nano
- * @param {string} opts.key API key to initialize Nano
- * @param {number} [opts.mode=0] Compression mode for Nano. Can be 0 = image mode or 1 = object mode
- * @namespace
- */
 function Nano(opts) {
     this.key = opts.key;
     this.mode = opts.mode || 0;
 }
 
-/**
- * Compress a SVG in string format and returns compress SVG in string format
- * @memberOf Nano
- * @param {string|File} file Can be either a string representing a SVG or an object with said string with size
- * @returns {Promise<File>}
- * @example
- * var Nano = require('nanosvg'),
- *     nano = new Nano({ key: <YOUR API KEY> });
- *
- *     compress('<svg>...</svg>').then(function(res){
- *         console.log(res); // { str: '<svg>...</svg>', size: ... }
- *     });
- */
-Nano.prototype.compress =  function(file) {
-    var me = this,
-        obj = {};
+Nano.prototype.compress = function (src, tgt, opts) {
+    _glob(src).then(function (files) {
+        if (files.length === 0) { console.error("No files found"); }
 
-    if (typeof file === 'string') {
-        obj.str = file;
-        obj.size = file.length;
-        obj.mode = me.mode;
-    }
-    else {
-        obj.str = file.str;
-        obj.size = file.size;
-        obj.mode = file.mode || me.mode;
-    }
-
-    return new Promise(function (resolve, reject) {
-        if (me.key) {
-            _request(
-                {
-                    method: 'POST',
-                    url: URL,
-                    headers: {
-                        'content-type': 'application/json',
-                        'x-api-key': me.key
-                    },
-                    body: { file: file },
-                    json: true
-                }, function (err, res, body) {
-                    if (err) { reject(err); }
-                    else {
-                        if (res && res.statusCode !== 200) { reject(body); }
-                        else {
-                            resolve(body);
-                        }
-                    }
-                });
-        }
-        else {
-            reject('No API key provided.');
-        }
+        compressBatch(files, tgt, opts);
     });
-};
 
-/**
- * Compress svg(s) from a directory and output to a target directory
- * @memberOf Nano
- * @param {string} src Glob pattern representing source files.
- * @param {string} tgt Glob pattern representing target directory.
- * @example
- * var Nano = require('nanosvg'),
- *     nano = new Nano({ key: <YOUR API KEY> });
- *
- *     nano.compressFiles('./uncompressed/*.svg', './compressed/');
- *     // Compressed: 0.svg, 0/2
- *     //  Savings: 38.87%
- *     // Compressed: 1.svg, 1/2
- *     //  Savings: 70.32%
- *     // Compressed: 2.svg, 2/2
- *     //  Savings: 69.99%
- */
-Nano.prototype.compressFiles = function(src, tgt) {
-    var me = this;
+    function compressBatch(files, tgt, opts) {
+        var LIMIT = 100,
+            i = 0,
+            tasks = [];
 
-    if (me.key) {
-        _glob(_path.join(CWD, src)).then(function (all_paths) {
-            var i = 0,
-                total_length = all_paths.length;
+        opts = opts || {};
+        opts._total = opts._total || files.length;
+        opts._index = opts._index || 0;
 
-            if (total_length === 0) { console.error('No files found'); }
+        for (; i < LIMIT; i++) {
+            if (files[i]) {
+                tasks.push(
+                    compressFile(files[i], tgt, opts).then(function (file) {
+                        opts._index++;
 
-            all_paths = splitArray(all_paths, 5); //break paths into 5 each
+                        _fs.outputFileSync(_path.join(CWD, tgt, file.name), file.str);
 
-            all_paths.forEach(function (paths, index) {
-                setTimeout(function () {
+                        console.log('Compressed: ' + file.name + ' ' + //eslint-disable-line no-console
+                            opts._index + '/' + opts._total +
+                            ' ' + (file.old_size / 1024).toFixed(1) + 'KB -> ' + (file.size /1024).toFixed(1) + 'KB' +
+                            ' ' + (((file.old_size - file.size) / file.old_size) * 100).toFixed(2) + '% saved');
 
-                    paths.forEach(function (path) {
-                        readFile(path).then(function (file) {
-                            file.mode = file.mode || me.mode;
+                    }).catch(function (err) {
+                        opts._index++;
+                        console.error('Compression failed (' + err.name + ') : ', err.msg);
+                        return;
+                    })
+                );
+            }
+        }
 
-                            return me.compress(file).then(function (res) {
-                                _fs.outputFileSync(_path.join(CWD, tgt, file.name), res.str);
-                                console.log('Compressed: ' + file.name + ', ' + i++ + '/' + total_length);
-                                console.log('\x1b[36m', 'Savings: ' + (((file.size - res.size) / file.size) * 100).toFixed(2) + '%', '\x1b[0m');
-                            });
+        files.splice(0, LIMIT);
 
-                        }).catch(function (err) {
-                            console.error(err);
-                        });
-                    });
-
-                }, index * 1000);
-            });
-        }).catch(function (err) { console.error(err); });
-    }
-    else { console.error('No API key provided.'); }
-
-    function splitArray(arr, size) {
-        var ret = [];
-
-        while (arr.length) { ret.push(arr.splice(0, size)); }
-
-        return ret;
+        Promise.all(tasks).then(function () {
+            if (files.length > 0) {
+                compressBatch(files, tgt, opts);
+            }
+        });
     }
 
-    function readFile(path) {
+    function compressFile(src, tgt, opts) {
         return new Promise(function (resolve, reject) {
-            Promise.all([
-                _fs.stat(path),
-                _fs.readFile(path, 'utf8')
-            ]).then(function (ret) {
-                var file = {},
-                    stat = ret[0],
-                    str = ret[1];
+            _fs.stat(src).then(function (stats) {
+                var file = {};
 
-                if (stat.isFile()) {
-                    file.name = _path.basename(path);
-                    file.size = stat.size;
-                    file.str = str;
+                if (stats.isFile()) {
+                    file.name = _path.basename(src);
+                    file.size = stats.size;
+                    file.mode = opts.mode || []; //0 === IMG mode, 1 === OBJ mode
 
-                    resolve(file);
+                    _fs.readFile(src, 'utf8').then(function (str) {
+                        file.str = str;
+
+                        _request({
+                            method: 'POST',
+                            url: URL,
+                            headers: {
+                                'content-type': 'application/json',
+                                'x-api-key': opts.key
+                            },
+                            body: { file: file },
+                            json: true
+                        }, function (err, res, body) {
+                            if (err) { reject(err); }
+                            else {
+                                if (res.statusCode !== 200) {
+                                    reject({ msg: res.body.error, name: file.name });
+                                }
+                                else {
+                                    body.name = file.name;
+                                    body.old_size = file.size;
+                                    resolve(body);
+                                }
+                            }
+                        });
+                    }).catch(function (err) {
+                        reject(err);
+                    });
                 }
-                else { reject('Not a valid file'); }
-            }).catch(reject);
+                else { reject({ msg: 'Not a valid SVG file.' }) }
+            });
         });
     }
 };
@@ -173,7 +110,7 @@ Nano.prototype.compressFiles = function(src, tgt) {
 if (args._.length < 2) { console.log('Usage: nanofy [options] input output'); }
 else {
     nano = new Nano(args);
-    nano.compressFiles(args._[0], args._[1]);
+    nano.compress(args._[0], args._[1], args);
 }
 
 module.exports = Nano;
