6083c47fa52fc6b7ab6b626e5b6728a5e688542d
neel <neel2292@gmail.com>
2019-05-02 10:15:13 +0800

    Minor changes

--- index.js
+++ index.js
@@ -70,51 +70,53 @@ Nano.prototype.compress = function (src, tgt, opts) {
             return;
         });
     }
-
-    function compressFile(src, tgt, opts) {
-        return new Promise(function (resolve, reject) {
-            _fs.stat(src).then(function (stats) {
-                var file = {};
-
-                if (stats.isFile()) {
-                    file.name = _path.basename(src);
-                    file.size = stats.size;
-                    file.mode = opts.mode || 0;
-
-                    _fs.readFile(src, 'utf8').then(function (str) {
-                        file.str = str;
-
-                        _request({
-                            method: 'POST',
-                            url: URL,
-                            headers: {
-                                'content-type': 'application/json',
-                                'x-api-key': opts.key
-                            },
-                            body: { file: file },
-                            json: true
-                        }, function (err, res, body) {
-                            if (err) { reject(err); }
-                            else {
-                                if (res.statusCode !== 200) {
-                                    reject({ msg: res.body.error, name: file.name });
-                                }
-                                else {
-                                    body.name = file.name;
-                                    body.old_size = file.size;
-                                    resolve(body);
-                                }
-                            }
-                        });
-                    }).catch(function (err) {
-                        reject(err);
-                    });
-                }
-                else { reject({ msg: 'Not a valid SVG file.' }) }
-            });
-        });
-    }
 };
+
+Nano.prototype.compressFile = compressFile;
+
+function compressFile(src, tgt, opts) {
+    return new Promise(function (resolve, reject) {
+        _fs.stat(src).then(function (stats) {
+            var file = {};
+
+            if (stats.isFile()) {
+                file.name = _path.basename(src);
+                file.size = stats.size;
+                file.mode = opts.mode || 0;
+
+                _fs.readFile(src, 'utf8').then(function (str) {
+                    file.str = str;
+
+                    _request({
+                        method: 'POST',
+                        url: URL,
+                        headers: {
+                            'content-type': 'application/json',
+                            'x-api-key': opts.key
+                        },
+                        body: { file: file },
+                        json: true
+                    }, function (err, res, body) {
+                        if (err) { reject(err); }
+                        else {
+                            if (res.statusCode !== 200) {
+                                reject({ msg: res.body.error, name: file.name });
+                            }
+                            else {
+                                body.name = file.name;
+                                body.old_size = file.size;
+                                resolve(body);
+                            }
+                        }
+                    });
+                }).catch(function (err) {
+                    reject(err);
+                });
+            }
+            else { reject({ msg: 'Not a valid SVG file.' }) }
+        });
+    });
+}
 
 //called on CLI
 if (require.main === module) {
